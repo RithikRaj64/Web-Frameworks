3d16e36a42c7743c389f9d8e9dcea98107225ed3
Rithik Raj <100361240+RithikRaj64@users.noreply.github.com>
2023-02-21 13:54:05 +0530

    Initial

--- Day1/Qn1/springapp/src/main/java/com/example/springapp/Controllers/ApiController.java
+++ Day1/Qn1/springapp/src/main/java/com/example/springapp/Controllers/ApiController.java
@@ -1,14 +1,13 @@
 package com.example.springapp.Controllers;
 
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class ApiController {
 
-    @RequestMapping("/favColor")
-    public String getMyFav(@RequestBody String yourFavColor) {
-        return "My Favourite color is " + yourFavColor;
+    @GetMapping("/welcome")
+    public String welcome() {
+        return "Welcome String Boot!";
     }
 }
